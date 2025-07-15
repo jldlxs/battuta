0b1006892f23955034794614349ccb3343580930
Batutta33 <anselmo33cba@gmail.com>
2025-07-15 14:32:30 -0400

    Correções solicitadas

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,7 +6,8 @@ import {
   IonHeader,
   IonIcon,
   IonSpinner,
-  ToastController
+  ToastController,
+  IonInput
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -19,7 +20,7 @@ import {
     FormsModule,
     IonContent,
     IonHeader,
-    IonSpinner
+    IonSpinner, IonInput
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
@@ -28,28 +29,14 @@ export class LoginPage implements OnInit {
   public password: string = '';
   public showError: boolean = false;
   public isLoading: boolean = false;
-  public toastMessage: string = '';
-  public toastColor: string = 'primary';
 
-  constructor(private toastController: ToastController) {}
+
+
+  constructor() {}
 
   ngOnInit() {}
 
-  async presentToast(message: string, color: string = 'primary') {
-    const toast = await this.toastController.create({
-      message,
-      duration: 3000,
-      position: 'top',
-      color
-    });
-    await toast.present();
-  }
 
-  showRestrictedToast() {
-    this.toastMessage = 'Funcionalidade de acesso restrito solicitada.';
-    this.toastColor = 'primary';
-    this.presentToast(this.toastMessage, this.toastColor);
-  }
 
   onLogin() {
     this.isLoading = true;
@@ -59,24 +46,22 @@ export class LoginPage implements OnInit {
 
       if (!this.username || !this.password) {
         this.showError = true;
-        this.toastMessage = 'Por favor, preencha todos os campos.';
-        this.toastColor = 'danger';
-        this.presentToast(this.toastMessage, this.toastColor);
+
+
         return;
       }
 
       if (this.username !== 'admin' || this.password !== '123456') {
         this.showError = true;
-        this.toastMessage = 'Dados de acesso não encontrados. Verifique os dados informados.';
-        this.toastColor = 'danger';
-        this.presentToast(this.toastMessage, this.toastColor);
+
+
+
         return;
       }
 
       this.showError = false;
-      this.toastMessage = 'Acesso autorizado com sucesso!';
-      this.toastColor = 'success';
-      this.presentToast(this.toastMessage, this.toastColor);
+
+
 
       // Redirecionamento pode ser colocado aqui se necessário
     }, 1000);
