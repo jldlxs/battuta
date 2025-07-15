e84a271007f6fcff7514ba99da7a0a09a5908d11
bettina-btt <bettina@battuta.com.br>
2025-07-15 14:05:54 -0400

    feature/menu lateral

--- src/app/components/menu/menu-list/menu-list.component.ts
+++ src/app/components/menu/menu-list/menu-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { IonList, IonItemGroup, MenuController, } from '@ionic/angular/standalone';
 import { MenuItemComponent } from '../menu-item/menu-item.component';
 import { MenuItem } from 'src/app/types/menu-item';
