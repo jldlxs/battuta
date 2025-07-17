2e8503bc69f545082bbe75d294369b2731d7191f
Batutta33 <anselmo33cba@gmail.com>
2025-07-17 12:44:30 -0400

    atualização de import IonButton.

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonSpinner, IonInput } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonSpinner, IonInput,IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, IonContent, IonHeader, IonSpinner, IonInput],
+  imports: [CommonModule, FormsModule, IonContent, IonHeader, IonSpinner, IonInput, IonButton],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginPage implements OnInit {
