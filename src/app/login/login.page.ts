1c4e9b55dbe01b9234ee740d7854b28261f1286e
Jeandre Lucas <jldlxs@gmail.com>
2025-07-21 22:50:16 -0400

    ajustes e criação do componente (company-selector)

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonSpinner, IonInput,IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonSpinner, IonInput, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
