6f528698fd2410ad39f39fb5edc7a9662434e8e8
bettina-btt <bettina@battuta.com.br>
2025-07-22 09:19:35 -0400

    commit empty

--- src/app/manager/manager.page.ts
+++ src/app/manager/manager.page.ts
@@ -19,6 +19,7 @@ import { MenuListComponent } from '../components/menu/menu-list/menu-list.compon
   standalone: true,
 <<<<<<< Updated upstream
   imports: [CommonModule, FormsModule, IonSplitPane, IonMenu, IonRouterOutlet, IonHeader, IonToolbar, IonContent, IonText, IonCard, IonFooter, IonButtons, IonIcon, MenuListComponent],
+  imports: [CommonModule, FormsModule, IonSplitPane, IonMenu, IonRouterOutlet, IonHeader, IonToolbar, IonContent, IonText, IonCard, IonFooter, IonButtons, IonIcon, MenuListComponent,],
 })
 export class ManagerPage implements OnInit {
   /**
