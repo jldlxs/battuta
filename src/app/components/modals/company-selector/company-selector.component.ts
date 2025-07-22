f41224874241c1065cdd0c91fd4295e65eef1c93
Jeandre Lucas <jldlxs@gmail.com>
2025-07-21 22:54:10 -0400

    criação do componente da modal company-selector

---
+++ src/app/components/modals/company-selector/company-selector.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-selector',
+  templateUrl: './company-selector.component.html',
+  styleUrls: ['./company-selector.component.scss'],
+})
+export class CompanySelectorComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
