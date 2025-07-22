af3fad5e9d9eda6ce4b1e3d336916c3f95cfd428
bettina-btt <bettina@battuta.com.br>
2025-07-22 09:19:58 -0400

    Update manager.page.ts

--- src/app/manager/manager.page.ts
+++ src/app/manager/manager.page.ts
@@ -17,11 +17,7 @@ import { MenuListComponent } from '../components/menu/menu-list/menu-list.compon
   templateUrl: './manager.page.html',
   styleUrls: ['./manager.page.scss'],
   standalone: true,
-<<<<<<< Updated upstream
   imports: [CommonModule, FormsModule, IonSplitPane, IonMenu, IonRouterOutlet, IonHeader, IonToolbar, IonContent, IonText, IonCard, IonFooter, IonButtons, IonIcon, MenuListComponent],
-=======
-  imports: [CommonModule, FormsModule, IonSplitPane, IonMenu, IonRouterOutlet, IonHeader, IonToolbar, IonContent, IonText, IonCard, IonFooter, IonButtons, IonIcon, MenuListComponent,],
->>>>>>> Stashed changes
 })
 export class ManagerPage implements OnInit {
   /**
